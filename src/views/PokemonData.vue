250ec2cf98340e8f3af1abf766265e344c7cb61e
yagox <yago.ramiresx@gmail.com>
2023-02-10 17:03:33 -0300

    feat: fetch de pokemon individual

--- src/views/PokemonData.vue
+++ src/views/PokemonData.vue
@@ -1,23 +1,111 @@
 <template>
-  <div class="about">
-    <h1>{{ route.params.id }}</h1>
-  </div>
+  <main v-if="!loading">
+    <div class="pokemonCard">
+      <img :src="pokemonData.value?.sprites.front_default" alt="pokemon" class="pokemonCard__image" />
+      <h1 class="pokemonCard__title">{{ pokemonData.value?.name }}</h1>
+    </div>
+    <div class="evolutionsCard">
+      <h2>Evoluções</h2>
+      <div class="evolutionContainer">
+        <div v-for="(pokemon, index) in evolutionData" :key="index" class="evolutionContainer__card">
+          <router-link :to="'/pokemon/' + pokemon.id">
+            <img :src="pokemon.image" alt="pokemon" class="evolutionContainer__image" />
+            <h2 class="evolutionContainer__title">{{ pokemon.name }}</h2>
+          </router-link>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script setup>
-import { useRoute } from "vue-router";
-
-
+import { reactive, ref } from 'vue';
+import { useRoute, RouterLink } from 'vue-router';
+import axios from 'axios';
 const route = useRoute();
-console.log(route.params.id)
+
+const baseUrl = 'https://pokeapi.co/api/v2';
+
+const loading = ref(false);
+const pokemonData = reactive({});
+const evolutionData = ref([]);
+
+const fetchPokemonData = async () => {
+  loading.value = true;
+  const fetchData = await axios.get(`${baseUrl}/pokemon/${route.params.id}`);
+  pokemonData.value = fetchData.data;
+  fetchSpecies(pokemonData.value.species.url);
+  loading.value = false;
+};
+const fetchSpecies = async (url) => {
+  const fetchData = await axios.get(url);
+  fetchEvolutions(fetchData.data.evolution_chain.url);
+};
+
+const fetchEvolutions = async (url) => {
+  const fetchData = await axios.get(url);
+
+  let evolvesTo
+  if (fetchData.data.chain.evolves_to.length > 1) {
+    evolvesTo = fetchData.data.chain.evolves_to;
+    const getUrl = evolvesTo.map((pokemon) => pokemon.species.url)
+    getUrl.forEach(data => fetchEvolutionData(data))
+  } else {
+    const getUrl = [
+      fetchData.data.chain.evolves_to[0].species.url,
+      fetchData.data.chain.evolves_to[0].evolves_to[0].species.url,
+    ]
+    getUrl.forEach(data => fetchEvolutionData(data))
+  }
+};
+
+const fetchEvolutionData = async (url) => {
+  const fetchData = await axios.get(url.replace("pokemon-species", "pokemon"));
+  const data = { name: fetchData.data.name, id: fetchData.data.id, image: fetchData.data.sprites.front_default }
+  // console.log(data)
+  evolutionData.value = [...evolutionData.value, data]
+  return data
+}
+
+fetchPokemonData();
 </script>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+<style lang="scss">
+@use '../assets/main.scss' as *;
+
+.pokemonCard {
+  @include flexCenter;
+  flex-direction: column;
+  padding: 2rem;
+}
+
+.pokemonCard__title {
+  color: $yellow;
+  text-transform: capitalize;
+  font-weight: bold;
+}
+
+.pokemonCard__image {
+  width: 150px;
+}
+
+.evolutionContainer__image {
+  width: 100px;
+}
+
+.evolutionContainer {
+  @include flexCenter;
+  gap: 1rem;
+}
+
+.evolutionContainer__card {
+  @include flexCenter;
+  flex-direction: column;
+  cursor: pointer;
+}
+
+.evolutionContainer__title {
+  color: $yellow;
+  text-transform: capitalize;
 }
 </style>
--- src/views/PokemonList.vue
+++ src/views/PokemonList.vue
@@ -1,13 +1,20 @@
 <template>
   <main>
-
     <div class="listContainer">
       <h2 class="title">Pokemons</h2>
 
       <div>
         <ul class="list">
           <li v-for="(pokemon, index) in pokemons" :key="index">
-            <RouterLink :to="{ name: 'pokemon', params: { id: index + 1 } }" class="list___item">
+            <RouterLink :to="{
+              name: 'pokemon',
+              params: {
+                id: pokemon.url.replace(
+                  'https://pokeapi.co/api/v2/pokemon/',
+                  ''
+                ),
+              },
+            }" class="list___item">
               {{ pokemon.name }}
             </RouterLink>
           </li>
@@ -15,54 +22,54 @@
       </div>
 
       <div class="buttonContainer">
-        <button v-if="prevPage !== null" @click="loadPrevPage" class="buttonContainer__btn">{{
-          fetchingPokemon? '...': 'Voltar'
-        }}</button>
-        <button @click="loadNextPage" class="buttonContainer__btn">{{
-          fetchingPokemon? '...': 'Próxima'
-        }}</button>
+        <button v-if="prevPage !== null" @click="loadPrevPage" class="buttonContainer__btn">
+          {{ fetchingPokemon? '...': 'Voltar' }}
+        </button>
+        <button @click="loadNextPage" class="buttonContainer__btn">
+          {{ fetchingPokemon? '...': 'Próxima' }}
+        </button>
       </div>
     </div>
   </main>
 </template>
 
 <script>
-import { defineComponent, ref, onMounted } from 'vue'
-import api from '@/services/api'
-import axios from 'axios'
+import { defineComponent, ref, onMounted } from 'vue';
+import api from '@/services/api';
+import axios from 'axios';
 
 export default defineComponent({
   name: 'PokemonList',
   setup() {
-    const pokemons = ref([])
-    let nextPage = ref('')
-    let prevPage = ref('')
+    const pokemons = ref([]);
+    let nextPage = ref('');
+    let prevPage = ref('');
     const fetchPokemons = async () => {
-      const fetchData = await api.get('/pokemon')
-      nextPage.value = fetchData.data.next
-      prevPage.value = fetchData.data.previous
-      pokemons.value = (fetchData.data.results)
-    }
+      const fetchData = await api.get('/pokemon');
+      nextPage.value = fetchData.data.next;
+      prevPage.value = fetchData.data.previous;
+      pokemons.value = fetchData.data.results;
+    };
 
     const loadNextPage = async () => {
-      const fetchData = await axios.get(nextPage.value)
-      pokemons.value = (fetchData.data.results)
-      nextPage.value = fetchData.data.next
-      prevPage.value = fetchData.data.previous
-    }
+      const fetchData = await axios.get(nextPage.value);
+      pokemons.value = fetchData.data.results;
+      nextPage.value = fetchData.data.next;
+      prevPage.value = fetchData.data.previous;
+    };
 
     const loadPrevPage = async () => {
-      const fetchData = await axios.get(prevPage.value)
-      pokemons.value = (fetchData.data.results)
-      nextPage.value = fetchData.data.next
-      prevPage.value = fetchData.data.previous
-    }
+      const fetchData = await axios.get(prevPage.value);
+      pokemons.value = fetchData.data.results;
+      nextPage.value = fetchData.data.next;
+      prevPage.value = fetchData.data.previous;
+    };
 
     onMounted(fetchPokemons);
 
-    return { pokemons, loadNextPage, loadPrevPage, prevPage, nextPage }
-  }
-})
+    return { pokemons, loadNextPage, loadPrevPage, prevPage, nextPage };
+  },
+});
 </script>
 
 <style lang="scss" scoped>
@@ -104,9 +111,9 @@ export default defineComponent({
   margin-top: 2rem;
   border: none;
   color: #fff;
-  border-radius: .5rem;
+  border-radius: 0.5rem;
   background: $yellow;
-  transition: all .3s;
+  transition: all 0.3s;
   padding: 1rem;
   cursor: pointer;
 }
